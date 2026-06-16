1f16ca24df06cfc31c922e705c9d1fa7ec9e1b8c
klaus <klaus.hoehfurtner@gmail.com>
2026-06-16 08:15:15 +0200

    OMVF Path angepasst

--- EV3.sh
+++ EV3.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+OVMF_CODE=$(find /nix/store -path '*OVMF*/FV/OVMF_CODE.fd' 2>/dev/null | head -n 1)
+
 
 qemu-system-x86_64 \
   -enable-kvm \
@@ -6,7 +8,7 @@ qemu-system-x86_64 \
   -device virtio-vga \
   -m 8192 \
   -smp 4 \
-  -drive if=pflash,format=raw,readonly=on,file=/nix/store/zp51g43pr03sisri6xs1nyba6j2ad5kc-OVMF-202602-fd/FV/OVMF_CODE.fd \
+  -drive if=pflash,format=raw,readonly=on,file="$OVMF_CODE" \
   -drive if=pflash,format=raw,file=VM/EV3/REV3_WS20262027.nvram \
   -drive file=VM/EV3/REV3_WS20262027.qcow2,format=qcow2,if=ide \
   -drive file=VM/virtio-win-0.1.285.iso,media=cdrom
